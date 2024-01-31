577e5a81a01213ffec5765d8e15be75378175f39
Mukotso <kelvinmukotso@gmail.com>
2024-01-31 08:03:03 +0300

    string reversal exercise done

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,24 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    // solution 1
+    // return str.split('').reverse().join('')
+    
+    // solution 2
+    // let reversedString = ""
+
+    // for( let char of str){
+    //     reversedString = char + reversedString
+    // }
+
+    // return reversedString
+
+    // solution 3
+
+  return  str.split('').reduce((reversedString, character)=>{
+            return character + reversedString
+    }, "")
+}
 
 module.exports = reverse;
