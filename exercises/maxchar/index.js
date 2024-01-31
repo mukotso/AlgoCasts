b2482e7ea0e6937893b034dde9e48a14d89e6d24
Mukotso <kelvinmukotso@gmail.com>
2024-01-31 23:19:27 +0300

    find the max char in a string done

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let charsMap = {}
+    let max = 0
+    let maxChar = ""
+
+    for (let char of str) {
+        if (charsMap[char]) {
+            charsMap[char]++
+        } else {
+            charsMap[char] = 1
+        }
+    }
+
+    for ( let ch in charsMap){
+        if(charsMap[ch] > max){
+            max = charsMap[ch]
+            maxChar = ch
+
+        }
+    }
+
+    return maxChar
+}
 
 module.exports = maxChar;
