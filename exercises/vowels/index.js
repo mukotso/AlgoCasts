d46929eda4fc268467e37ee50ebfc50bc89d43b0
Mukotso <kelvinmukotso@gmail.com>
2024-02-02 07:41:39 +0300

    vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,14 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    const vowels = ["a","e", "i", "o", "u"];
+   return str.split('').reduce((count, char)=>{
+      if(vowels.includes(char.toLowerCase())){
+       return count++
+      }
+      return count
+    }, 0)
+}
 
 module.exports = vowels;
